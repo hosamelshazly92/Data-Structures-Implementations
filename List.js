3912103214cc5edb3d29ffb41b73104d5b69e102
Hosam Elshazly <hosamelshazly92@gmail.com>
2021-08-11 16:25:25 +0200

    create list methods: insert and clear

--- List.js
+++ List.js
@@ -1,26 +1,28 @@
-// List ADT
-// a list is an ordered sequence of data
+/*
+List ADT
+a list is an ordered sequence of data
 
-// the complete List ADT
-// +------------+----------+--------------------------------------------------+
-// | name       | type     | description                                      |
-// +------------+----------+--------------------------------------------------+
-// | listSize   | property | Number of elements in list                       |
-// | pos        | property | Current position in list                         |
-// | length     | property | Returns the number of elements in list           |
-// | clear      | function | Clears all elements from list                    |
-// | toString   | function | Returns string representation of list            |
-// | getElement | function | Returns element at current position              |
-// | insert     | function | Inserts new element after existing element       |
-// | append     | function | Adds new element to end of list                  |
-// | remove     | function | Removes element from list                        |
-// | front      | function | Sets current position to first element of list   |
-// | end        | function | Sets current position to last element of list    |
-// | prev       | function | Moves current position back one element          |
-// | next       | function | Moves current position forward one element       |
-// | currPos    | function | Returns the current position in list             |
-// | moveTo     | function | Moves the current position to specified position |
-// +------------+----------+--------------------------------------------------+
+the complete List ADT
++------------+----------+--------------------------------------------------+
+| name       | type     | description                                      |
++------------+----------+--------------------------------------------------+
+| listSize   | property | Number of elements in list                       |
+| pos        | property | Current position in list                         |
+| length     | property | Returns the number of elements in list           |
+| clear      | function | Clears all elements from list                    |
+| toString   | function | Returns string representation of list            |
+| getElement | function | Returns element at current position              |
+| insert     | function | Inserts new element after existing element       |
+| append     | function | Adds new element to end of list                  |
+| remove     | function | Removes element from list                        |
+| front      | function | Sets current position to first element of list   |
+| end        | function | Sets current position to last element of list    |
+| prev       | function | Moves current position back one element          |
+| next       | function | Moves current position forward one element       |
+| currPos    | function | Returns the current position in list             |
+| moveTo     | function | Moves the current position to specified position |
++------------+----------+--------------------------------------------------+
+*/
 
 class List {
     constructor() {
@@ -64,5 +66,23 @@ class List {
             return true;
         }
         return false;
+    }
+
+    // insert an element to the list
+    insert(idx, elm) {
+        let foundAt = this.find(idx);
+        if(foundAt > -1) {
+            this.dataStore.splice(foundAt + 1, 0, elm);
+            ++this.listSize;
+            return true;
+        }
+        return false;
+    }
+
+    // remove all elements from a list
+    clear() {
+        delete this.dataStore;
+        this.dataStore = [];
+        this.listSize = this.pos = 0;
     }
 }
