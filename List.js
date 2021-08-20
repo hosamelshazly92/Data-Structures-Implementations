39cef5decc44b3596270dbe9d4acbcd86bded55d
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-08-20 16:43:52 +0200

    create list class iterator

--- List.js
+++ List.js
@@ -1,28 +1,4 @@
-/*
-List ADT
-a list is an ordered sequence of data
-
-the complete List ADT
-+------------+----------+--------------------------------------------------+
-| name       | type     | description                                      |
-+------------+----------+--------------------------------------------------+
-| listSize   | property | Number of elements in list                       |
-| pos        | property | Current position in list                         |
-| length     | property | Returns the number of elements in list           |
-| clear      | function | Clears all elements from list                    |
-| toString   | function | Returns string representation of list            |
-| getElement | function | Returns element at current position              |
-| insert     | function | Inserts new element after existing element       |
-| append     | function | Adds new element to end of list                  |
-| remove     | function | Removes element from list                        |
-| front      | function | Sets current position to first element of list   |
-| end        | function | Sets current position to last element of list    |
-| prev       | function | Moves current position back one element          |
-| next       | function | Moves current position forward one element       |
-| currPos    | function | Returns the current position in list             |
-| moveTo     | function | Moves the current position to specified position |
-+------------+----------+--------------------------------------------------+
-*/
+// List ADT (Abstract Data Structure)
 
 class List {
     constructor() {
@@ -43,8 +19,8 @@ class List {
 
     // find an element in the list
     find(elm) {
-        for(let i = 0; i < this.dataStore.length; i++) {
-            if(this.dataStore[i] == elm) {
+        for (let i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i] == elm) {
                 return i;
             }
         }
@@ -60,7 +36,7 @@ class List {
     remove(elm) {
         let foundAt = this.find(elm);
 
-        if(foundAt > -1) {
+        if (foundAt > -1) {
             this.dataStore.splice(foundAt, 1);
             --this.listSize;
             return true;
@@ -71,7 +47,7 @@ class List {
     // insert an element to the list
     insert(idx, elm) {
         let foundAt = this.find(idx);
-        if(foundAt > -1) {
+        if (foundAt > -1) {
             this.dataStore.splice(foundAt + 1, 0, elm);
             ++this.listSize;
             return true;
@@ -88,8 +64,8 @@ class List {
 
     // determine if a value is in the list
     contain(elm) {
-        for(let i = 0; i < this.dataStore.length; ++i) {
-            if(this.dataStore[i] == elm) {
+        for (let i = 0; i < this.dataStore.length; ++i) {
+            if (this.dataStore[i] == elm) {
                 return true;
             }
         }
@@ -98,11 +74,11 @@ class List {
 
     // traverse the list
     front() {
-        return this.pos = 0;
+        return (this.pos = 0);
     }
 
     end() {
-        return this.pos = this.listSize - 1;
+        return (this.pos = this.listSize - 1);
     }
 
     currPos() {
@@ -110,22 +86,22 @@ class List {
     }
 
     prev() {
-        if(this.pos > 0) {
+        if (this.pos > 0) {
             return --this.pos;
         }
         return this.pos;
     }
 
     next() {
-        if(this.pos < this.listSize - 1) {
+        if (this.pos < this.listSize - 1) {
             return ++this.pos;
         }
         return false;
     }
 
     moveTo(position) {
-        if(position <= this.listSize - 1) {
-            return this.pos = position;
+        if (position <= this.listSize - 1) {
+            return (this.pos = position);
         }
         return false;
     }
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Data-Structures-Implementations
+
 JavaScript data structures implementations repository
+
+## List ADT (Abstract Data Type)
+
+a list is an ordered sequence of data
+
+### Usage
+
+| name       |   type   | description                                      |
+| :--------- | :------: | :----------------------------------------------- |
+| listSize   | property | Number of elements in list                       |
+| pos        | property | Current position in list                         |
+| length     | property | Returns the number of elements in list           |
+| clear      | function | Clears all elements from list                    |
+| toString   | function | Returns string representation of list            |
+| getElement | function | Returns element at current position              |
+| insert     | function | Inserts new element after existing element       |
+| append     | function | Adds new element to end of list                  |
+| remove     | function | Removes element from list                        |
+| front      | function | Sets current position to first element of list   |
+| end        | function | Sets current position to last element of list    |
+| prev       | function | Moves current position back one element          |
+| next       | function | Moves current position forward one element       |
+| currPos    | function | Returns the current position in list             |
+| moveTo     | function | Moves the current position to specified position |
+
+create list class implementation
+
+```javascript
+let movies = new List();
+
+movies.append("The Dark Knight");
+movies.append("Star Wars");
+movies.append("The Matrix");
+movies.append("The Godfather");
+movies.append("Fight Club");
+movies.append("Inception");
+
+console.log(movies.toString());
+```
+
+create list class iterator
+
+```javascript
+for (let i = movies.currPos(); i < movies.length; i++) {
+    console.log(movies.getElement());
+    movies.next();
+}
+```
